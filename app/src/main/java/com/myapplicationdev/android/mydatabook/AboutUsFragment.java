6f860fd60ddf4c3637f0509ae30ec9e0ba8165cb
JonathanZYZ <jonathanzengyuzhao@gmail.com>
2021-06-08 11:02:53 +0800

    About us

--- app/src/main/java/com/myapplicationdev/android/mydatabook/AboutUsFragment.java
+++ app/src/main/java/com/myapplicationdev/android/mydatabook/AboutUsFragment.java
@@ -1,5 +1,6 @@
 package com.myapplicationdev.android.mydatabook;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 public class AboutUsFragment extends Fragment {
@@ -25,7 +27,18 @@ public class AboutUsFragment extends Fragment {
         iv = view.findViewById(R.id.iv);
 
         String imageUrl = "https://upload.wikimedia.org/wikipedia/commons/8/80/Republic_Polytechnic_Logo.jpg";
-        Picasso.with(getActivity()).load(imageUrl).into(iv);
+        //Picasso.with(getActivity()).load(imageUrl).into(iv);
+
+        Picasso.Builder builder = new Picasso.Builder(getActivity());
+        builder.listener(new Picasso.Listener()
+        {
+            @Override
+            public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception)
+            {
+                exception.printStackTrace();
+            }
+        });
+        builder.build().load(imageUrl).into(iv);
 
         return view;
     }
