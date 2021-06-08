b5c554e3411f5781e7ef7ff6a3ff9a41ea68fa33
Nix WM <16046530@myrp.edu.sg>
2021-06-08 14:48:39 +0800

    Done padding for every xml cause qi kai ask me to.

--- app/src/main/java/com/myapplicationdev/android/mydatabook/AboutUsActivity.java
+++ app/src/main/java/com/myapplicationdev/android/mydatabook/AboutUsActivity.java
@@ -22,6 +22,7 @@ public class AboutUsActivity extends AppCompatActivity {
         String imageUrl = "https://upload.wikimedia.org/wikipedia/commons/8/80/Republic_Polytechnic_Logo.jpg";
 
         // .placeholder to hold the loader image when the imageUrl is still loading.
+        // . error to display error.png if the image is fails to load.
         Glide.with(AboutUsActivity.this)
                 .load(imageUrl)
                 .apply(new RequestOptions().placeholder(R.drawable.ajax_loader).error(R.drawable.error))
